b53eacbe9e20d231e766ba1b7bfdea528217fe7c
Federico Brigante <me@fregante.com>
2023-12-14 17:45:49 +0800

    Initial version

---
+++ cli.js
@@ -0,0 +1,154 @@
+#!/usr/bin/env node
+import process from 'node:process';
+import {createServer} from 'node:http';
+import * as p from '@clack/prompts';
+import open from 'open';
+import getPort from 'get-port';
+import pDefer from 'p-defer';
+
+const approvalCode = pDefer();
+const port = await getPort();
+const localhost = '127.0.0.1';
+
+// TODO: Remove after https://github.com/natemoo-re/clack/issues/181
+const tasks = async tasks => {
+	for (const task of tasks) {
+		if (task.enabled === false) {
+			continue;
+		}
+
+		const s = p.spinner();
+		s.start(task.title);
+		// eslint-disable-next-line no-await-in-loop -- Sequential
+		const result = await task.task(s.message);
+		s.stop(result || task.title);
+	}
+};
+
+const server = createServer((request, response) => {
+	const {searchParams} = new URL(request.url, serverUrl);
+	if (searchParams.has('code')) {
+		approvalCode.resolve(searchParams.get('code'));
+		// Html header
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.end('You can close this tab now. <script>window.close()</script>');
+		server.close();
+		return;
+	}
+
+	response.writeHead(400, {'Content-Type': 'text/plain'});
+	response.end('No `code` found in the URL. WHO R U?');
+});
+
+// Start the server on port 3000
+server.listen(port, localhost);
+
+const serverUrl = `http://${localhost}:${port}`;
+
+function required(input) {
+	if (input.trim() === '') {
+		return 'Required';
+	}
+}
+
+async function getRefreshToken() {
+	const request = await fetch('https://accounts.google.com/o/oauth2/token', {
+		method: 'POST',
+		body: new URLSearchParams([
+			['client_id', group.clientId],
+			['client_secret', group.clientSecret],
+			['code', code],
+			['grant_type', 'authorization_code'],
+			['redirect_uri', serverUrl], // Unused but required
+		]),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+	});
+
+	if (!request.ok) {
+		throw new Error('Error while getting the refresh token: ' + request.statusText);
+	}
+
+	const response = await request.json();
+
+	if (response.error) {
+		throw new Error('Error while getting the refresh token: ' + response.error);
+	}
+
+	return response.refresh_token;
+}
+
+function getLoginUrl(clientId) {
+	const url = new URL('https://accounts.google.com/o/oauth2/auth');
+	url.searchParams.set('response_type', 'code');
+	url.searchParams.set('access_type', 'offline');
+	url.searchParams.set('client_id', clientId);
+	url.searchParams.set('scope', 'https://www.googleapis.com/auth/chromewebstore');
+	url.searchParams.set('redirect_uri', serverUrl);
+	return url.href;
+}
+
+p.intro('Follow the steps at this URL to generate the API keys, then enter them below to generate the refresh token.\n   https://github.com/fregante/chrome-webstore-upload-keys');
+const group = await p.group(
+	{
+		// ExtensionId: () => p.text({
+		// 	message: 'Extension ID:',
+		// 	placeholder: 'e.g. bdeobgpddfaegbjfinhldnkfeieakdaf, it’s in the Chrome Web Store URL',
+		// }),
+		clientId: () => p.text({
+			message: 'Client ID:',
+			placeholder: 'e.g. 960453266371-2qcq5fppm3d5e.apps.googleusercontent.com',
+			validate: required,
+		}),
+		clientSecret: () => p.text({
+			message: 'Client secret:',
+			placeholder: 'e.g. GOCSPX-O9uS1FLnCqXDvru7Y_',
+			validate: required,
+		}),
+		open: () => p.confirm({
+			message: 'Open the authentication page in the default browser?',
+		}),
+	},
+	{
+		onCancel() {
+			p.cancel('Operation cancelled.');
+			process.exit(0); // `onCancel` continues to the next question
+		},
+	},
+);
+
+let code;
+let refreshToken;
+await tasks([
+	{
+		title: 'Opening the login page in the browser',
+		async task() {
+			if (group.open) {
+				await open(getLoginUrl(group.clientId));
+				return 'Page opened';
+			}
+
+			return 'Continue in: ' + getLoginUrl(group.clientId);
+		},
+	},
+	{
+		title: 'Waiting for you to complete the process in the browser. Follow its steps and warnings (this is your own personal app)',
+		async task() {
+			code = await approvalCode.promise;
+			return 'Approval code received from Google';
+		},
+	},
+	{
+		title: 'Asking Google for the refresh token',
+		async task() {
+			refreshToken = await getRefreshToken();
+			return `Done:
+
+CLIENT_ID=${group.clientId}
+CLIENT_SECRET=${group.clientSecret}
+REFRESH_TOKEN=${refreshToken}
+`;
+		},
+	},
+]);
---
+++ readme.md
@@ -0,0 +1,68 @@
+# How to generate Google API keys
+
+> Guide and OAuth helper to generate keys
+
+Companion to [Web Store Upload](https://github.com/fregante/chrome-webstore-upload).
+
+You can follow this complete guide or the official-but-partial one at: https://developer.chrome.com/docs/webstore/using-api
+
+> [!TIP]
+> The names you enter here don't really matter. It's an app that only you will have access to. This will take approximately 10 minutes and Google likes to change these screens often. Sorry.
+
+1. Visit https://console.developers.google.com/apis/credentials
+0. Create a project:
+
+	> <img width="772" alt="Google APIs: Create project" src="https://user-images.githubusercontent.com/1402241/77865620-9a8a3680-722f-11ea-99cb-b09e5c0c11ec.png">
+
+0. Enter `chrome-webstore-upload` and **Create**
+0. Visit https://console.cloud.google.com/apis/credentials/consent
+0. Select on **External** and **Create**
+
+	> <img width="804" alt="OAuth Consent Screen" src="https://user-images.githubusercontent.com/1402241/133878019-f159f035-2b76-4686-a461-0e0005355da6.png">
+
+0. Only enter the Application name (e.g. `chrome-webstore-upload`) and required email fields, and click **Save**
+
+	> <img width="475" alt="Consent screen configuration" src="https://user-images.githubusercontent.com/1402241/77865809-82ff7d80-7230-11ea-8a96-e381d55524c5.png">
+
+0. On the 3rd screen, add your own email address:
+
+	> <img width="632" alt="Test users selection" src="https://user-images.githubusercontent.com/1402241/106213510-7c180300-6192-11eb-97b4-b4ae92424bf1.png">
+
+0. Visit https://console.developers.google.com/apis/library/chromewebstore.googleapis.com
+0. Click **Enable**
+0. Visit https://console.developers.google.com/apis/credentials
+0. Click **Create credentials** > **OAuth client ID**:
+
+	> <img width="771" alt="Create credentials" src="https://user-images.githubusercontent.com/1402241/77865679-e89f3a00-722f-11ea-942d-5245091f22b8.png">
+
+0. Select **Desktop app**, enter `Chrome Webstore Upload` and click **Create**
+
+	> <img width="568" alt="Create OAuth client ID" src="https://user-images.githubusercontent.com/1402241/163124196-c4bb4f26-9766-4766-bb81-3982875d3a84.png">
+
+0. Save your ✅ `clientId` and ✅ `clientSecret`:
+
+	> <img width="554" alt="OAuth client created" src="https://user-images.githubusercontent.com/1402241/228934028-1ef55a41-cc92-4ecf-967a-1984a363c21d.png">
+
+0. Visit https://console.cloud.google.com/apis/credentials/consent
+0. Click **PUBLISH APP** and confirm
+
+	> <img width="771" alt="Publish app" src="https://user-images.githubusercontent.com/27696701/114265946-2da2a280-9a26-11eb-9567-c4e00f572500.png">
+
+0. Run this CLI tool to generate the required `refreshToken`
+  ```sh
+  npx chrome-webstore-upload-keys
+  ```
+  or
+  ```sh
+  bunx chrome-webstore-upload-keys
+  ```
+
+	> <img width="771" alt="chrome-webstore-upload-keys demo" src="./demo.gif">
+
+9001. Done. Now you should have ✅ `clientId`, ✅ `clientSecret` and ✅ `refreshToken`. You can use these for all your extensions, but don't share them!
+
+## What the CLI tool does
+
+1. Requests the two keys you have
+2. Opens a local server to handle the OAuth redirect from Google's servers with an `approvalCode`
+3. Uses Google's `oauth2` API to request a `refreshToken`
